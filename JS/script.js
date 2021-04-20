a00921913ecd6c375ebd986b5aef491bdbb07ec0
EmmanueleLibraro <emmanuelelibraro95@gmail.com>
2021-04-20 16:17:55 +0200

    fine

--- JS/script.js
+++ JS/script.js
@@ -4,6 +4,6 @@ const app = new Vue ({
     data: {
         title: 'New World',
         image: 'https://newworlditalia.it/wp-content/uploads/2020/11/new_world_italia_logo.png',
-        imag: 'https://www.google.com/search?q=new+world+image&sxsrf=ALeKk03ag19lMgVwF9fHhFUsU62Za7tbbw:1618926121738&source=lnms&tbm=isch&sa=X&ved=2ahUKEwielZ3j-YzwAhWIqaQKHSriD48Q_AUoAXoECAEQAw&biw=1920&bih=969#imgrc=ni5g05_hJk5GUM'
+        imag: 'https://images-na.ssl-images-amazon.com/images/I/71WXjM75u2L._AC_SX679_.jpg'
     }
 });
