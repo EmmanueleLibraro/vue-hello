680351d46fc55d6582c12776dd5471ecf8baedf9
EmmanueleLibraro <emmanuelelibraro95@gmail.com>
2021-04-20 20:04:26 +0200

    fine

--- JS/script.js
+++ JS/script.js
@@ -3,6 +3,7 @@ const app = new Vue ({
     el: '#app',
     data: {
         title: 'New World',
+        titl: 'Per la closed beta mancano...',
         image: 'https://newworlditalia.it/wp-content/uploads/2020/11/new_world_italia_logo.png',
         imag: 'https://images-na.ssl-images-amazon.com/images/I/71WXjM75u2L._AC_SX679_.jpg',
         img: 'https://images.ctfassets.net/j95d1p8hsuun/7v9lFYQ6gXUJIKdOmXU00D/338ce46e280d57706ec6e9d497bdb62d/EditionSelector_DeluxeEdition-530x330.jpg',
